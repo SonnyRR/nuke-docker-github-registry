6cefe5458a92d987d898094ece8157bb5078b1d9
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2022-12-30 22:19:03 +0200

    Introduced new sample API.
    Includes built-in & traditional container support with a `Dockerfile`.

---
+++ MagicEightBall.API/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Magic 8-Ball API",
+        Description = "An ASP.NET Core Web API for answering questions via an 8-Ball like mechanism.",
+        TermsOfService = new Uri("https://localhost"),
+        Contact = new OpenApiContact
+        {
+            Name = "Vasil Kotsev",
+            Url = new Uri("https://github.com/SonnyRR")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://github.com/SonnyRR/nuke-docker-github-registry/blob/main/LICENSE")
+        }
+    });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
