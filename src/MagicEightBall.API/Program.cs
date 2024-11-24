aa6110861d1906f01af5e97b239b2abe5e540d22
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2024-11-24 11:18:52 +0200

    Bump API TFM to net9.0

--- src/MagicEightBall.API/Program.cs
+++ src/MagicEightBall.API/Program.cs
@@ -19,28 +19,28 @@ builder.Services.AddHttpLogging(logging => logging.LoggingFields = HttpLoggingFi
 
 builder.Services.AddSwaggerGen(options =>
 {
-    string gitHubProfile = builder.Configuration["Links:GitHub"] ?? string.Empty;
-    string license = builder.Configuration["Links:License"] ?? string.Empty;
+	string gitHubProfile = builder.Configuration["Links:GitHub"] ?? string.Empty;
+	string license = builder.Configuration["Links:License"] ?? string.Empty;
 
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Magic 8-Ball API",
-        Description = "An ASP.NET Core Web API for answering questions via an 8-Ball like mechanism.",
-        Contact = new OpenApiContact
-        {
-            Name = "Vasil Kotsev",
-            Url = new Uri(gitHubProfile)
-        },
-        License = new OpenApiLicense
-        {
-            Name = "MIT License",
-            Url = new Uri(license)
-        }
-    });
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Magic 8-Ball API",
+		Description = "An ASP.NET Core Web API for answering questions via an 8-Ball like mechanism.",
+		Contact = new OpenApiContact
+		{
+			Name = "Vasil Kotsev",
+			Url = new Uri(gitHubProfile)
+		},
+		License = new OpenApiLicense
+		{
+			Name = "MIT License",
+			Url = new Uri(license)
+		}
+	});
 
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+	var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
 var app = builder.Build();
@@ -50,4 +50,5 @@ app.UseSwagger();
 app.UseRewriter(new RewriteOptions().AddRewrite("^$", "swagger", true));
 app.UseSwaggerUI();
 app.MapControllers();
+
 await app.RunAsync();
