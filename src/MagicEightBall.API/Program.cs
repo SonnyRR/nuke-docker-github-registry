62349683b17f67da5235bdffbbd748a03585ea01
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2025-11-22 11:03:18 +0200

    Move to MinimalAPI & Scalar

--- src/MagicEightBall.API/Program.cs
+++ src/MagicEightBall.API/Program.cs
@@ -1,54 +1,55 @@
 using System;
-using System.IO;
-using System.Reflection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddHttpLogging(logging => logging.LoggingFields = HttpLoggingFields.All);
-
-builder.Services.AddSwaggerGen(options =>
-{
-	string gitHubProfile = builder.Configuration["Links:GitHub"] ?? string.Empty;
-	string license = builder.Configuration["Links:License"] ?? string.Empty;
-
-	options.SwaggerDoc("v1", new OpenApiInfo
-	{
-		Version = "v1",
-		Title = "Magic 8-Ball API",
-		Description = "An ASP.NET Core Web API for answering questions via an 8-Ball like mechanism.",
-		Contact = new OpenApiContact
-		{
-			Name = "Vasil Kotsev",
-			Url = new Uri(gitHubProfile)
-		},
-		License = new OpenApiLicense
-		{
-			Name = "MIT License",
-			Url = new Uri(license)
-		}
-	});
-
-	var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-	options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseHttpLogging();
-app.UseSwagger();
-app.UseRewriter(new RewriteOptions().AddRewrite("^$", "swagger", true));
-app.UseSwaggerUI();
-app.MapControllers();
+app.MapOpenApi();
+app.MapScalarApiReference(
+    "/api/documentation",
+    options =>
+    {
+        options.WithTitle("Magic 8-Ball API");
+        options.WithOperationTitleSource(OperationTitleSource.Path);
+        options.SortTagsAlphabetically();
+    });
+
+var possibleAnswers = new[]
+{
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "It is certain.",
+    "It is decidedly so."
+};
+
+var random = new Random();
+
+app.MapPost("/api/question/ask", (string q) =>
+{
+    if (string.IsNullOrWhiteSpace(q))
+        return Results.BadRequest();
+
+    var answer = possibleAnswers[random.Next(possibleAnswers.Length - 1)];
+    return Results.Ok(answer);
+})
+.WithName("AskQuestion")
+.WithSummary("Answers a question provided by the user")
+.WithDescription("Provides a random Magic 8-Ball answer to a question");
 
 await app.RunAsync();
